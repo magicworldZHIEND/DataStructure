148a3398167ee3a3b628c797caa4b176545638c0
ZHIEND <74717815+magicworldZHIEND@users.noreply.github.com>
2023-04-09 17:45:18 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@DataStructure/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("this is my first project");
+  printf("Hello World\n");
+
+  for (int i = 10; i <= 29; i++) {
+    printf("Thanks\n");
+  }
+  return 0;
+}
